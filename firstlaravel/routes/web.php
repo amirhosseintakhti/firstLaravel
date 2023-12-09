377d0cea91832ae2badb655c843dea5fcafc4074
AmirHossein Takhti <amirhosseintakhti.at@gmail.com>
2023-12-09 16:38:31 +0330

    changing view instead of get

--- firstlaravel/routes/web.php
+++ firstlaravel/routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 Route::get('/contact', function () {
     return view('contact');
 });
